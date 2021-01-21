7de92a1b26fd02fa34b1e16de823d7b9aec94639
Dario Correal <dcorreal@uniandes.edu.co>
2021-01-20 21:09:53 -0500

    documentacion del modelo

--- App/model.py
+++ App/model.py
@@ -30,44 +30,44 @@ from DISClib.ADT import list as lt
 assert cf
 
 """
-Se define la estructura de un catálogo de libros.
-El catálogo tendrá tres listas, una para libros, otra para autores
-y otra para géneros
-"""
+En el modelo, se crean las estructuras de datos, es decir,
+las variables donde se van a guardar los datos leidos de los
+archivos CSV.
 
-# Construccion de modelos
+El modelo debe ser el unico sitio donde se modifican y manipulan
+los datos.
+"""
 
 
 def addBooks(booksfile):
     """
-    Inicializa el catálogo de libros. Crea una lista vacia para guardar
-    todos los libros, adicionalmente, crea una lista vacia para los autores,
-    una lista vacia para los generos y una lista vacia para la asociación
-    generos y libros. Retorna el catalogo inicializado.
+    Para guardar los libros provenientes del archivo CSV
+    vamos a crear una lista, en donde quedarán todos los datos.
+
+    No es importante entender como funciona esta lista por ahora.
+
+    La funcion newList crea una lista de muchas formas.  Una de ellas
+    es leyendo todo lo que encuentre en el archivo indicado por filename.
+    Cada linea del archivo quedará en una posicion de la lista.
     """
     books = lt.newList(datastructure='SINGLE_LINKED',
                        filename=booksfile)
     return books
 
 
-def newTag(name, id):
-    """
-    Esta estructura almancena los tags utilizados para marcar libros.
-    """
-    tag = {'name': '', 'tag_id': ''}
-    tag['name'] = name
-    tag['tag_id'] = id
-    return tag
-
-
 def addTag(taglist, tag):
     """
-    Adiciona un tag a la lista de tags
+    Para procesar el archivo de tags vamos a usar de otra forma la lista.
+    En este caso, agregaremos cada linea del archivo a la lista, en lugar
+    de usar la opcion de crearla con el nombre del archivo.
     """
-    t = newTag(tag['tag_name'], tag['tag_id'])
-    lt.addLast(taglist, t)
+    lt.addLast(taglist, tag)
 
 
 def createTagList():
+    """
+    Esta funcion crea una lista vacia.  Esta lista se utilizara
+    para ir guardando la informacion en el archivo de tags.
+    """
     taglist = lt.newList(datastructure='SINGLE_LINKED')
     return taglist
