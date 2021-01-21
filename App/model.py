31dc08ee1e6cef6c8c5761c971bbf88efb4b846d
Dario Correal <dcorreal@uniandes.edu.co>
2021-01-20 20:33:25 -0500

    version inicial

---
+++ App/model.py
@@ -0,0 +1,73 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación,
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contribuciones:
+ *
+ * Dario Correal - Version inicial
+ """
+
+
+import config as cf
+from DISClib.ADT import list as lt
+assert cf
+
+"""
+Se define la estructura de un catálogo de libros.
+El catálogo tendrá tres listas, una para libros, otra para autores
+y otra para géneros
+"""
+
+# Construccion de modelos
+
+
+def addBooks(booksfile):
+    """
+    Inicializa el catálogo de libros. Crea una lista vacia para guardar
+    todos los libros, adicionalmente, crea una lista vacia para los autores,
+    una lista vacia para los generos y una lista vacia para la asociación
+    generos y libros. Retorna el catalogo inicializado.
+    """
+    books = lt.newList(datastructure='SINGLE_LINKED',
+                       filename=booksfile)
+    return books
+
+
+def newTag(name, id):
+    """
+    Esta estructura almancena los tags utilizados para marcar libros.
+    """
+    tag = {'name': '', 'tag_id': ''}
+    tag['name'] = name
+    tag['tag_id'] = id
+    return tag
+
+
+def addTag(taglist, tag):
+    """
+    Adiciona un tag a la lista de tags
+    """
+    t = newTag(tag['tag_name'], tag['tag_id'])
+    lt.addLast(taglist, t)
+
+
+def createTagList():
+    taglist = lt.newList(datastructure='SINGLE_LINKED')
+    return taglist
